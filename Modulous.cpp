dc5f79938873c016e91baa9852abae352b0811b0
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-04-09 11:12:49 +0545

    Add files via upload

---
+++ Modulous.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main (){
+	int a,b,S;
+printf("Enter a value of a:");
+scanf("%d",&a);
+printf("Enter a value of b:");
+scanf("%d",&b);
+S=a%b;
+printf("Total is %d ",S);
+getch();
+}
+
