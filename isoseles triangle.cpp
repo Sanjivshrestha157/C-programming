944ad310030ebe7be73fc128ce16c4f37ab818f2
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-04-24 10:35:27 +0545

    Add files via upload

---
+++ isoseles triangle.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c;
+	printf("Enter a value of a:\n",a);
+	scanf("%d",&a);
+	printf("Enter a value of b:\n",b);
+	scanf("%d",&b);
+	printf("Enter a value of c:\n",c);
+	scanf("%d",&c);
+	if((a==b)||(b==c))
+	{
+		printf("The given triangle is isoseles triangle");
+	}
+	else
+	{
+		printf("The given triangle is not isoseles triangle");
+	}
+	getch();
+}
