173891b6df2e2552fe9178e69f6029ce8923bfba
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-06 11:13:10 +0545

    Add files via upload

---
+++ Enter your choice.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int ch;
+	printf("\n**menu**");
+	printf("\n1.Circle");
+	printf("\n2.Rectangle");
+	printf("\n3.Square");
+	printf("\n4.Triangle");
+	printf("\nEnter your choice");
+}
