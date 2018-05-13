c4fe150f2485e7ec7382f6e116e0154381b7ecfb
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-13 10:42:31 +0545

    Add files via upload

---
+++ comparing youngest and oldest.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int age1,age2,age3;
+	int young,old;
+	printf("Enter ages of three people:");
+	scanf("%d%d%d",&age1,&age2,&age3);
+	young=age1<age2?(age1<age3?age1:age3):(age2<age3?age2:age3);
+	old=age1>age2?(age1>age3?age1:age3):(age2>age3?age2:age3);
+	printf("%d is the youngest\n",young);
+	printf("%d is the oldest\n",old);
+	getch();
+}
