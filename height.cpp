b59c9bdd765cc8bf3987e51de2825d41f36f84b6
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-13 11:24:18 +0545

    Add files via upload

--- height.cpp
+++ height.cpp
@@ -1,13 +1,14 @@
+/*lab 6 3nd one*/
 #include<stdio.h>
 #include<conio.h>
 main()
 {
-	float feet1,feet2,inch1,inch2;
+	int feet1,feet2,inch1,inch2;
 	float feet3,inch3;
 	printf("Enter height in feet of two people:");
-	scanf("%d%d%d",&feet1,&feet2);
+	scanf("%d%d",&feet1,&feet2);
 	printf("Enter height in inch of two people:");
-	scanf("%d%d%d",&inch1,&inch2);
+	scanf("%d%d",&inch1,&inch2);
 	feet3=feet1+feet2;
 	inch3=inch1+inch2;
 	if(inch3>12)
@@ -15,4 +16,6 @@ main()
 		feet3=feet3+feet1;
 		inch3=inch3-12;
 	}
+	printf("the total height is %f feet%f inch",feet3,inch3);
+	getch();
 }
