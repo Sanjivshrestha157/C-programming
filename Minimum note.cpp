cd65a5d5fddbf675a067ae4dee0a431ab206189d
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-17 11:16:24 +0545

    lab7

---
+++ Minimum note.cpp
@@ -0,0 +1,12 @@
+/*Lab 7*/
+/*Wap to input amount from user and print minimum no. of notes reauired from user*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int amt;
+	if(amt>=1000)
+	{
+		amt1000=amt/1000;
+		amt=amt%1000;
+	}
