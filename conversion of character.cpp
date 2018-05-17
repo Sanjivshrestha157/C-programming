68f404a46848af16bbfa0e23962670fef9a46248
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-17 10:58:57 +0545

    Lab 7

---
+++ conversion of character.cpp
@@ -0,0 +1,21 @@
+/*Lab 7*/
+/* Wap to convert case of character from user*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+	int ch;
+	printf("\nEnter character:");
+	scanf("%c",&ch);
+	if((ch>='a')&&(ch<='z'))
+	{
+	ch=ch-32;
+	}
+	else if((ch>='A')&&(ch<='Z'))
+	{
+	ch=ch+32;
+    }
+    printf("output character is %c",ch);
+    getch();
+}
