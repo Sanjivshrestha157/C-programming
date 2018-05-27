d5a221ed480f682330102c71e2e8a64e9239a024
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-26 22:10:43 -0700

    lab 9

---
+++ print all alphabet.cpp
@@ -0,0 +1,14 @@
+/*for(initialization;test condition;increment/decrement*/
+/*Lab 9*/
+/*Wap to print all alphabet from a to z */
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char ch;
+	for(ch='a';ch<='z';ch++)
+	{
+		printf("%c\n",ch);
+	}
+	getch();
+}
