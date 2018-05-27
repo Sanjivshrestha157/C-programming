b650234f8c4a7b1fcfd2664f97eea6876050971f
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-26 22:00:10 -0700

    Lab 9

---
+++ print 20-1.cpp
@@ -0,0 +1,14 @@
+/*for(initialization;test condition;increment/decrement*/
+/*Lab 9*/
+/*Wap to print all numbers from 1 to 20*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	for(i=20;i>=1;i--)
+	{
+		printf("%d\n",i);
+	}
+	getch();
+}
