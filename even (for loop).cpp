02aa843ae7cab380773c9c5feb4b1ec7d56afe0d
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-05-26 22:05:59 -0700

    lab 9

---
+++ even (for loop).cpp
@@ -0,0 +1,14 @@
+/*for(initialization;test condition;increment/decrement*/
+/*Lab 9*/
+/*Wap to print all numbers from 1 to 20*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	for(i=20;i>=2;i=i-2)
+	{
+		printf("%d\n",i);
+	}
+	getch();
+}
