ba1a93c0b7fab9fd940075a67e72fca7d7fe784d
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-06-05 11:20:42 +0545

    Lab 9

---
+++ atm(do while).cpp
@@ -0,0 +1,30 @@
+/*WAP to implement login scenario of an ATM.The program should ask the user to enter the pin number until pin is correct, once the pin number is correct display the welcome msg to user.Pin number is 1234*/
+/*Lab 9*/
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+main()
+{
+	int p,count=0;
+	do
+	{
+	 system("cls");
+	 if(count>0)
+	 {
+	 	printf("Invalid pin");
+	 }
+	 printf("\nEnter your pin number:");
+	 scanf("%d",&p);
+	 count++;
+	}while(p!=1234&&count<3);
+	system("cls");
+	if(count<3)
+	{
+	 printf("Welcome");
+	}
+	else if(count<=3)
+	{
+		printf("You have entered the pin three times wrong");
+	}
+	getch();
+} 
