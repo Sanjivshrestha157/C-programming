4ce60d73bba84106186590309cdaebfbeb5314bb
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-06-05 10:32:17 +0545

    Update pin number(do while).cpp

--- pin number(do while).cpp
+++ pin number(do while).cpp
@@ -1,4 +1,5 @@
 /*WAP to implement login scenario of an ATM.The program should ask the user to enter the pin number until pin is correct, once the pin number is correct display the welcome msg to user.Pin number is 1234*/
+/*Lab 9*/
 #include<stdio.h>
 #include<conio.h>
 main()
