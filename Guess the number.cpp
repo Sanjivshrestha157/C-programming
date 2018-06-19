5421c215d5e0acff7d89280de82bc87b4a8df2b4
Sanjivshrestha157 <38194670+Sanjivshrestha157@users.noreply.github.com>
2018-06-19 10:31:34 +0545

    Lab 10

--- Guess the number.cpp
+++ Guess the number.cpp
@@ -1,1 +1,19 @@
-/*WAP named'guess the number'
+/*WAP named'guess the number'*/
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+main()
+{
+	int a,i=1;
+	srand(time(NULL)%100);
+	do
+	{
+		system("cls");
+		printf("Enter any number:");
+		scanf("%d",&a);	
+		printf("%d\n",rand());
+		i++;
+	}while((a!=55)&i<10);
+	getch();
+}
